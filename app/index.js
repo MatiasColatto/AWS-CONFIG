caeda5ef2b4d28e972df41e3e5f9de62ae53f0d3
MatiasColatto <mimaco1234@gmail.com>
2023-11-02 16:41:21 -0300

    Adding .env example

--- app/index.js
+++ app/index.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000; // Puerto en el que se ejecutará la aplicación
+const port = process.env.APP_PORT || 3000;
 
-// Ruta para la página de inicio
-app.get('/', (req, res) => {
-  res.send('¡Bienvenido a mi aplicación web!');
-});
-
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en el puerto ${port}`);
 });
---
+++ config/aws.config.js
@@ -0,0 +1,7 @@
+module.exports=
+{
+    "accessKeyId": process.env.ACCESS_KEY_ID,
+    "secretAccessKey": process.env.SECRET_ACCESS_KEY,
+    "sessionToken": process.env.SESSION_TOKEN,
+    "region": process.env.REGION 
+  }
